2cc6a6ae672f8470e0596f9dd3eaa67c17c99200
Paul Serraino <pserraino99@gmail.com>
2016-06-04 21:50:25 -0500

    get users geoloactions

--- basketclient/frontend/app.js
+++ basketclient/frontend/app.js
@@ -1,6 +1,5 @@
 var app = angular.module('app', ['ngMaterial', 'ui.router']);
 
-// http://nominatim.openstreetmap.org/search?q=135+pilkington+avenue,+birmingham&format=json
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
@@ -21,9 +20,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
         })
 })
 
-app.controller('HomeCtrl', function ($scope, LocationService) {
-    $scope.submit = function() {
+app.controller('HomeCtrl', function ($scope, $http, LocationService) {
+    $scope.user = {};
 
+    $scope.submit = function(user) {
+        LocationService.get().then(function(res) {
+            $http.post()
+        })
     };
 });
 
@@ -32,13 +35,30 @@ app.controller('ResultsCtrl', function($scope) {
 });
 
 app.service('LocationService', function($http, $q) {
-    
+    function nativeGeoCoder() {
+        var defer = $q.defer();
+
+        navigator.geolocation.getCurrentPosition(function(res){
+            defer.resolve({
+                lat: res.coords.latitude,
+                lon: res.coords.longitude
+            });
+        });
+        return defer.promise;
+    }
+
+    function geoCode(address) {
+        // http://nominatim.openstreetmap.org/search?q=135+pilkington+avenue,+birmingham&format=json
+        // TODO: make this work with address
+        return nativeGeoCoder();
+    }
+
     return {
         get: function(address) {
-            if (address) {
+            if (!!address && typeof address === "string" && address !== "") {
                 return geoCode(address);
             }
-            return nativeGeoCoder(address);
+            return nativeGeoCoder();
         }
     }
 })
