2e47638b35e6d32b0cd041a30a961ab337defd2b
Mason Schoolfield <mason@schoolfield.net>
2016-06-04 23:32:10 -0500

    now reading from database, even though the lat/lon are hardcoded

--- basketserver/app.py
+++ basketserver/app.py
@@ -26,19 +26,10 @@ def handler(event, context):
     This function fetches content from mysql RDS instance
     """
 
-    item_count = 0
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-    with conn.cursor() as cur:
-        cur.execute("create table Employee3 ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
-        cur.execute('insert into Employee3 (EmpID, Name) values(1, "Joe")')
-        cur.execute('insert into Employee3 (EmpID, Name) values(2, "Bob")')
-        cur.execute('insert into Employee3 (EmpID, Name) values(3, "Mary")')
-        conn.commit()
-        cur.execute("select * from Employee3")
-        for row in cur:
-            item_count += 1
-            logger.info(row)
-            #print(row)
+    cursor.execute("call GetAmenities ('30.338477', '-97.718813');")
 
+    rows = cursor.fetchall()
 
-    return "Added %d items from RDS MySQL table" %(item_count)
+    return rows;
